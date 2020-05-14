85cbbf48d2e81523e0140968a7373b597f9b908f
Charlie Page <charliepage88@gmail.com>
2020-05-13 21:13:14 -0400

    m

--- rules.php
+++ rules.php
@@ -3,6 +3,7 @@
 // Available placeholders: :uc:vendor, :uc:package, :lc:vendor, :lc:package
 return [
     'src/MyPackage.php' => 'src/:uc:package.php',
+    'src/Field/FieldMyPackage.php' => 'src/Field/Field:upc:package.php',
     'config/mypackage.php' => 'config/:lc:package.php',
     'src/Facades/MyPackage.php' => 'src/Facades/:uc:package.php',
     'src/MyPackageServiceProvider.php' => 'src/:uc:packageServiceProvider.php',
